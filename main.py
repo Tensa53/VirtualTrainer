f609fb357ffd91e04b09c85af8582849d24d7b44
Daniele Fabiano <danieletensa53@protonmail.com>
2023-02-09 00:35:01 +0100

    Inversione della dichiarazione delle fitness

--- main.py
+++ main.py
@@ -13,18 +13,45 @@ from deap import tools
 import constants
 import eserciziPopulator
 
+#Atleta di esempio:
+#Nome: Gianni
+#Peso: 70kg
+#Altezza: 160cm
+#Mesi Esperienza: 0
+
+#conf di base
+#select: tournament
+#crossover: twoPoint
+#mutation: UniFormInt
+
+#sperimentare cambiando un parametro alla volta
+#esperimento 1:
+#select: truncation
+#crossover: twoPoint
+#mutation: UniFormInt
+#esperimento 2:
+#select: tournament
+#crossover: onePoint
+#mutation: UniFormInt
+#esperimento 3:
+#select: truncation
+#crossover: onePoint
+#mutation: UniFormInt
+
+
 myAtleta = atleta.Atleta()
 eserciziFactory = eserciziPopulator.eserciziPopulator(90)
 eserciziFactory.populateEsercizi()
-popSize = 10
-first = 0
-crossoverProbability = 0.5
-mutationProbability = 0.2
-low = 0
-up = 89
 
-def pareto(individual):
-    individual.fitness.values
+POPSIZE = 10
+NUMEX = 6
+MATPOOL = 4
+FIRST = False
+CXPB = 0.5
+NGEN = 5
+MTPB = 0.2
+LOW = 0
+UP = 89
 
 
 def calcoloSfida(individual):
@@ -89,7 +116,6 @@ def calcoloAffinita(individual):
 
 
 def evaluateInd(individual):
-
     avgsfida = calcoloSfida(individual)
     avgaffinita = calcoloAffinita(individual)
 
@@ -98,22 +124,20 @@ def evaluateInd(individual):
     #
     # input("Premere un tasto per continuare...")
 
-    return avgsfida, avgaffinita
+    return avgaffinita, avgsfida
 
 
-def configuraParametriAlgoritmo(numEsercizi):
-    IND_SIZE = numEsercizi
-
-    creator.create("FitnessMulti", base.Fitness, weights=(1.0, 2.0))
+def configuraParametriAlgoritmo():
+    creator.create("FitnessMulti", base.Fitness, weights=(1.0, 1.0))
     creator.create("Individual", list, fitness=creator.FitnessMulti)
 
     toolbox = base.Toolbox()
-    toolbox.register("attr_int", random.randint, low, up)
-    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=IND_SIZE)
+    toolbox.register("attr_int", random.randint, LOW, UP)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=NUMEX)
 
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("mate", tools.cxTwoPoint)
-    toolbox.register("mutate", tools.mutUniformInt, low=low, up=up, indpb=0.5)
+    toolbox.register("mutate", tools.mutUniformInt, low=LOW, up=UP, indpb=0.5)
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("evaluate", evaluateInd)
 
@@ -121,7 +145,9 @@ def configuraParametriAlgoritmo(numEsercizi):
 
 
 def algoritmoSemplice(toolbox):
-    pop = toolbox.population(n=popSize)
+    pop = toolbox.population(n=POPSIZE)
+
+    hof = tools.HallOfFame(4)
 
     print("Initial population:")
     for p in pop:
@@ -129,66 +155,80 @@ def algoritmoSemplice(toolbox):
 
     print()
 
-    for g in range(1):
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, 4)
+    for g in range(NGEN):
+        print("GEN " + str(g))
 
-        print("Post selection:")
-        for o in offspring:
-            print(str(o) + "" + str(o.fitness))
+        # SELEZIONE
+        offspring = toolbox.select(pop, MATPOOL)
 
-        print()
+        # print("Post selection:")
+        # for o in offspring:
+        #     print(str(o) + "" + str(o.fitness))
+        #
+        # print()
 
-        # Apply crossover on the offspring
+        # CROSSOVER
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < 0.5:
+            if random.random() < CXPB:
                 toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child2.fitness.values
 
-        print("Post crossover:")
-        for o in offspring:
-            print(str(o) + "" + str(o.fitness))
+        # print("Post crossover:")
+        # for o in offspring:
+        #     print(str(o) + "" + str(o.fitness))
+        #
+        # print()
 
-        print()
-
-        # Apply mutation on the offspring
+        # MUTAZIONE
         for mutant in offspring:
-            if random.random() < 0.2:
+            if random.random() < MTPB:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
 
-        print("Post mutation:")
-        for o in offspring:
-            print(str(o) + "" + str(o.fitness))
+        # print("Post mutation:")
+        # for o in offspring:
+        #     print(str(o) + "" + str(o.fitness))
+        #
+        # print()
 
-        print()
-
-        # Evaluate the individuals with an invalid fitness
+        # VALUTAZIONE
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
 
-        # The population is entirely replaced by the offspring
+        # MEMORIZZAZIONE DELLA NUOVA POPOLAZIONE
         pop[:] = offspring
 
-        print("Post finish:")
+        print("Post evaluation:")
         for p in pop:
             print(str(p) + "" + str(p.fitness))
+
+        print()
+
+        # MEMORIZZAZIONE DEL MIGLIOR INDIVIDUO
+        hof.update(pop)
+
+    for h in hof:
+        print(str(h) + "" + str(h.fitness))
+
+    print()
+
+    print("Best Individual: " + str(hof[0]) + str(hof[0].fitness))
 
     input("Premi un tasto per continuare...")
 
 
-def generaScheda(numEsercizi):
+def generaScheda():
 
-    if first == 1:
+    if FIRST:
         if myAtleta.isGraveMagrezzaorObeso():
             print("Il tuo attuale peso non ti permette di procedere con la generazione scheda")
             input("Premi un tasto per continuare...")
             return
 
-        toolbox = configuraParametriAlgoritmo(numEsercizi)
+        toolbox = configuraParametriAlgoritmo()
         scheda = algoritmoSemplice(toolbox)
 
     else:
@@ -208,8 +248,8 @@ def inserisciInfoAtleta():
     myAtleta.altezza = altezza
     myAtleta.mesiEsperienza = mesiEsperienza
 
-    global first
-    first = 1
+    global FIRST
+    FIRST = True
 
 
 def modificaInfoAtleta(n):
@@ -242,7 +282,6 @@ def menu():
         print("2. Modifica informazioni atleta")
         print("3. Mostra caratteristiche atleta")
         print("4. Genera una scheda esercizi")
-        print("5. Testing scheda")
         n = int(input())
 
         if n == 0:
@@ -253,10 +292,11 @@ def menu():
             myAtleta.calcolaCaratteristiche()
 
         elif n == 2:
-            if first == 1:
+            if FIRST:
                 print(myAtleta.printAtletaInfoPrompt())
                 n=int(input("Seleziona l'info da modificare: "))
                 modificaInfoAtleta(n)
+                myAtleta.calcolaCaratteristiche()
                 print("Informazioni aggiornate. Le tue caratteristiche sono cambiate")
                 input("Premi un tasto per continuare...")
             else:
@@ -264,7 +304,7 @@ def menu():
                 input("Premi un tasto per continuare...")
 
         elif n == 3:
-            if first == 1:
+            if FIRST:
                 print(myAtleta.printAtletaFull())
                 input("Premi un tasto per continuare...")
             else:
@@ -272,10 +312,7 @@ def menu():
                 input("Premi un tasto per continuare...")
 
         elif n == 4:
-            generaScheda(6)
-
-        elif n == 5:
-            testScheda()
+            generaScheda()
 
         else:
             print("Seleziona una voce valida")
